14c012d86b43fd80cdc712be1907c31ac781585b
Wuilson Felipe <felipepinillos15@gmail.com>
2026-01-23 23:30:31 -0500

    Preparar para Railway

--- app/database.py
+++ app/database.py
@@ -1,15 +1,17 @@
 from sqlalchemy import create_engine # Motor de base de datos
 from sqlalchemy.ext.declarative import declarative_base # Base para los modelos
 from sqlalchemy.orm import sessionmaker # Crear sesiones
+from app.config import get_settings
 
 
 #Ahora vamos a crear los modelos y las migraciones
 
 from fastapi import Depends # Dependencias, para inyectar la sesión de base de datos
 
-DATABASE_URL = "mysql+pymysql://root@localhost:3306/sistemalibreria"
+# Obtener la configuración desde variables de entorno
+settings = get_settings()
 
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(settings.DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
